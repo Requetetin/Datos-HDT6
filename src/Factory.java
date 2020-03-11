b4b0785e0da6f6b51c90f2c6edf0f857e610d094
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-03-11 15:47:38 -0600

    Update Factory.java

--- src/Factory.java
+++ src/Factory.java
@@ -3,7 +3,33 @@
  * @date 11/03/2020
  * Hoja de trabajo 6
  */
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Factory{
+
+	public Map<String, String> getMap(String mapType){
+
+		if(mapType == null){
+			System.out.println("-Se ha ingresado un valor no valido, por favor ingresar de nuevo...");
+			return null;
+		}
+
+		if(mapType.equalsIgnoreCase("HashMap")){
+			return new HashMap<String, String>();
+		}
+
+		if(mapType.equalsIgnoreCase("TreeMap")){
+			return new TreeMap<String, String>();
+		}
+
+		if(mapType.equalsIgnoreCase("LinkedHashMap")){
+			return new LinkedHashMap<String, String>();
+		}
+		System.out.println("-Se ha ingresado un valor no valido, por favor ingresar de nuevo...");
+		return null;
+	}
 	
 }
